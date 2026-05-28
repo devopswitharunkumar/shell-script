3fe82b507748f48918f489db30bc244174fb69bb
arunkumar261 <arunkumar261@gmail.com>
2026-05-28 19:10:40 +0530

    script by using variables

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Ramesh : Hello Suresh, Good Morning"
+echo "Suresh : Hi Ramesh, Good Morning"
+echo "Ramesh : How are you ?"
+echo "Suresh : I am fine, How are you ?"
+echo "Ramesh : I am also fine, Thanks for asking"
+echo "Suresh : thanks, Will meet again Bye Bye"
