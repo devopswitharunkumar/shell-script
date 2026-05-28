db6a4204953b5b64a4371f823e24c0f0ff58de4e
arunkumar261 <arunkumar261@gmail.com>
2026-05-28 20:05:46 +0530

    run shell inside and take output

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DATE=$(date)   
+
+echo "Todays date is ${DATE}"  --> use only double quotes no single quotes
+
+#DATE = $(date)    --> WRONG
+#DATE = "$(date)"    --> WRONG BUT DATE="$(date)"
