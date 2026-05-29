1e5704a53fb93c5d5104b7b11e2d866f30242a11
arunkumar261 <arunkumar261@gmail.com>
2026-05-29 18:40:39 +0530

    implemented log concept

--- 11-redirectcmdsoutputtonewfile.sh
+++ 11-redirectcmdsoutputtonewfile.sh
@@ -8,24 +8,25 @@ LOG_FILE="/tmp/$0-$Timestamp.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+Y="\e[0m"
 
 
 validate(){
     if [ $1 -ne 0 ]
     then
-        echo -e "Error :: $2 is $R failed"
+        echo -e "$R Error :: $2 is failed $N"
         exit 1
     else
-        echo -e "$2 is $G successful"
+        echo -e "$G $2 is successful $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo "ERROR :: You are nnot a root user. Pls get the access"
+    echo -e "$R ERROR :: You are nnot a root user. Pls get the access $N"
     exit 1
 else
-    echo "You are a root user"
+    echo -e "$G You are a root user $N"
 fi
 
 yum install mysql -y &>> $LOG_FILE
