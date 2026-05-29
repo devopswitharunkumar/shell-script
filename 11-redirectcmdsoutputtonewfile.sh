b0e4dd5b7473b1a4da7b0b5233e8678215f2cc8f
arunkumar261 <arunkumar261@gmail.com>
2026-05-29 18:41:21 +0530

    implemented colors test concept

--- 11-redirectcmdsoutputtonewfile.sh
+++ 11-redirectcmdsoutputtonewfile.sh
@@ -29,10 +29,10 @@ else
     echo -e "$G You are a root user $N"
 fi
 
-yum install mysql -y &>> $LOG_FILE
+yum install mysqsdl -y &>> $LOG_FILE
 
 validate $? "Installing MYSQL"
 
-yum install git -y &>> $LOG_FILE
+yum install giddt -y &>> $LOG_FILE
 
 validate $? "Installing GIT" 
