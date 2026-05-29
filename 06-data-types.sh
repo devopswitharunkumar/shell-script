4a2ab5bb22159dff30eb15fd0d1899fefe94f762
arunkumar261 <arunkumar261@gmail.com>
2026-05-29 17:35:59 +0530

    added different arguments

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -4,3 +4,8 @@ NUMBER1=$1
 NUMBER2=$2
 
 echo "Total amount is  $(($NUMBER1 + $NUMBER2))" 
+
+echo "All args passed : $@"
+echo "no of args passed : $#"
+echo "script name : $0"
+echo "exit status : $?"
