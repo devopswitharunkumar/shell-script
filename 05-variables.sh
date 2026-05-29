296eeffc36e6fdc23ef86346f5ee1f667a9a7bf0
arunkumar261 <arunkumar261@gmail.com>
2026-05-29 12:16:03 +0530

    added script files for datatypes and arrays and variable

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "Please enter ur  username : "
+read -s USERNAME
+
+# echo "USERNAME IS $USERNAME"
+
+echo "Please enter ur password :"
+read -s PASSWORD
+
+# echo "Password is $PASSWORD"
