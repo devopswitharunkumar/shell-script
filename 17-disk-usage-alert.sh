e0b77e43aeb8fb4df0c1ce8d67c5c154b6d573d5
arunkumar261 <arunkumar261@gmail.com>
2026-06-04 07:16:48 +0530

    disk usage concepts

--- 17-disk-usage-alert.sh
+++ 17-disk-usage-alert.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-Disk_usage=$(df -hT | gerp -vE 'tmp/File')
+Disk_usage=$(df -hT | grep -vE 'tmp/File')
 
 Disk_Thresold=1
 
 message=""
 
-while IFS = read line
+while IFS= read line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % 1F)
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $Disk_Thresold ]
     then 
-    message +="High Disk Usage on $partition : $usage\n"
+        message +="High Disk Usage on $partition : $usage\n"
     fi
 done <<< $Disk_usage
 
