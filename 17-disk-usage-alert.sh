88925ee4fd4f6a0cdeaa5609e499b14366024ada
arunkumar261 <arunkumar261@gmail.com>
2026-06-04 07:25:48 +0530

    disk usage concepts

--- 17-disk-usage-alert.sh
+++ 17-disk-usage-alert.sh
@@ -1,6 +1,26 @@
+# #!/bin/bash
+
+# Disk_usage=$(df -hT | grep -vE 'tmp/Filesystem')
+
+# Disk_Thresold=1
+
+# message=""
+
+# while IFS= read line
+# do
+#     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+#     partition=$(echo $line | awk '{print $1F}')
+#     if [ $usage -ge $Disk_Thresold ]
+#     then 
+#         message+="High Disk Usage on $partition: $usage\n"
+#     fi
+# done <<< $Disk_usage
+
+# echo -e "Message: $message"
+
 #!/bin/bash
 
-Disk_usage=$(df -hT | grep -vE 'tmp/Filesystem')
+Disk_usage=$(df -hT | grep -vE 'tmp|Filesystem')
 
 Disk_Thresold=1
 
@@ -8,12 +28,16 @@ message=""
 
 while IFS= read line
 do
-    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print $1F}')
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+
+    partition=$(echo $line | awk '{print $1}')
+
     if [ $usage -ge $Disk_Thresold ]
-    then 
-        message+="High Disk Usage on $partition: $usage\n"
+    then
+        message+="High Disk Usage on $partition: $usage%\n"
     fi
-done <<< $Disk_usage
+
+done <<< "$Disk_usage"
+
 
 echo -e "Message: $message"
