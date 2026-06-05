646c51ee6355868d14355934ac6fbbb6c8ff75be
arunkumar261 <arunkumar261@gmail.com>
2026-06-05 12:45:35 +0530

    corrected syntax in delete log script

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
+Source_dir="/tmp/shellscriptlogs/"
 
-$Source_dir="/tmp/shellscriptlogs/"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-$R="\e[31m"
-$G="\e[32m"
-$Y="\e[33m"
-$N="\e[0m"
-
-if [ ! d $Source_dir ]
+if [ ! -d $Source_dir ]
 then
     echo -e "$R ERROR :: Source directory does not exist $N"
 fi
 
 FILES_TO_DELETE=$(find $Source_dir -type f -mtime +14 -name "*.log")
 
-while IFS = read -r line
+while IFS= read -r line
 do
-    echo "Deleting file" $line
+    echo "Deleting file: $line"
     rm -rf $line
 done <<< $FILES_TO_DELETE
