a9012e80bde6d40781a3e90d9a0f2d70b9c0a8fc
arunkumar261 <arunkumar261@gmail.com>
2026-06-06 07:12:06 +0530

    getopt and optargs concept script

---
+++ 18-greeting-getopts-conncept.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+Name=""
+Wishes=""
+
+
+Usage(){
+    echo "Usage : $(basename $0) -n <name> -w <wishes>"
+    echo  "Options::"
+    echo " -n Specify the name (mandatory)"
+    echo " -w Specify the wishes. (Optional). Default is Good Morning"
+    echo " -h Display help and exit"
+}
+
+
+# while getopts "n:w:h" option
+# do 
+# ----
+# ---
+# done
+
+# or
+while getopts "n:w:h" opt; do
+    case $opt in
+        n) Name=$OPTARG;;
+        W) Wishes=$OPTARG;;
+        \?) echo "Invalid option: -"$OPTARG"" >&2; Usage ; exit;;
+        :) Usage ; exit;;
+        h) Usage; exit;;
+    esac 
+done
+
+
+
+if [ -z "$name" ] | [ -z "$Wishes"];  then
+    echo "Error : -n and -w are mandatory options"
+    Usage
+    exit
+fi
+
+echo "Hello $Name, $Wishes. I have been learning DevOps Course"
